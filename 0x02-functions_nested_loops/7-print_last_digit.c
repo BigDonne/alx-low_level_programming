3888fcaf341a8a7150e51f9214ffffb75882fb33
BigDonne <dccorbbi@gmail.com>
2022-12-14 15:52:27 -0800

    New colors and digits

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,17 +6,14 @@
  * @n: the number to compute last digit.
  * Return: value of the last digit.
  */
-
-
-
 int print_last_digit(int n)
 {
-	int last_digit = n % 10;
+	int ld = n % 10;
 
-	if (last_digit < 0)
+	if (ld < 0)
 	{
-		last_digit = -1;
+		ld = -1;
 	}
-	_putchar(last_digit + '0');
-	return (last_digit);
+	_putchar(ld + '0');
+	return (ld);
 }
