94c9e29e981a929bda9c781b8f216612e665d6a5
BigDonne <dccorbbi@gmail.com>
2022-12-19 14:20:37 -0800

    Copy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -5,16 +5,18 @@
  * @src: source
  * Return: string
  */
+
 char *_strcpy(char *dest, char *src)
 {
-	int len = 0;
+	int r = 0;
 
-	while (*(src + len) != '\0')
+	while (*(src + r) != '\0')
 	{
-		*(dest + len) != *(src + len);
-		len++;
+		*(dest + r) = *(src + r);
+		r++;
 	}
 
-	*(dest + len) = '\0';
+	*(dest + r) = '\0';
+
 	return (dest);
 }
