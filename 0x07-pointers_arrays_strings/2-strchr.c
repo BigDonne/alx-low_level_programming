fce07d659f33cce59cff11402595442efd9f286c
BigDonne <dccorbbi@gmail.com>
2023-01-03 19:48:48 -0800

    Set string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * set_string - sets the value of a pointer to a char
- * 2s: pointer to change
+ * @s: pointer to change
  * @to: string to change pointer to
  *
  * Return: void
