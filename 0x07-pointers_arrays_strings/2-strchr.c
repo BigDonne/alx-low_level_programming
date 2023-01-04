c0fa450d2db2894be8c58e99a33bc6fdab5a8843
BigDonne <dccorbbi@gmail.com>
2023-01-03 20:15:29 -0800

    New

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
  * set_string - sets the value of a pointer to a char
- * @s: pointer to change
- * @to: string to change pointer to
+ * @s: The string to be searched
+ * @to: The character to be located
  *
  * Return: void
  */
-void set_string(char **s, char *to)
+void set_string(char *s, char *to)
 {
 	*s = to;
 }
