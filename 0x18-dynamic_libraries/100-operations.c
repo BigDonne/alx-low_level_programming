49f8f07f74dc97f8e6c3e46d8135000aee08aef3
BigDonne <dccorbbi@gmail.com>
2023-03-20 22:31:24 -0700

    Updated solution

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * add - addtion
+ * @a: number
+ * @b: number
+ * Return: result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtraction
+ * @a: number
+ * @b: number
+ * Return: result
+ */
+int sub(int a, int b)
+{
+        return (a - b);
+}
+
+/**
+ * div - division
+ * @a: number
+ * @b: number
+ * Return: result
+ */
+int div(int a, int b)
+{
+        return (a / b);
+}
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
+/**
+ * mul - multiplication
+ * @a: number
+ * @b: number
+ * Return: result
+ */
+int mul(int a, int b)
+{
+        return (a * b);
+}
